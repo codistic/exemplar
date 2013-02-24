a2eb5b8c0696caee3f3311f27454c82071666b04
Jason Stehle <codistic@gmail.com>
2013-02-24 13:11:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Exemplar
+
+Exemplar is a documentation helper for JavaScript libraries.
+
+## Why?
+
+Have you ever use a library that obviously hasn't tested its examples? Here's how not to be that guy.
+
+#### Features
+
+* JavaScript object output
+* Raw HTML output
+* Rendered HTML output
+* Test output against expected results
+* Uses [SyntaxHighlighter](http://alexgorbatchev.com/SyntaxHighlighter/)
+
+
+## Get Started
+
+* [Project Home](http://www.codistic.com/projects/exemplar/)
+* [Docs](http://www.codistic.com/projects/exemplar/docs/)
+* [Examples](http://www.codistic.com/projects/exemplar/examples/)
+
+## IE Support
+Works on IE 9+.
+
+## Dependencies
+Exemplar requires [jQuery](http://jquery.com/) and [SyntaxHighlighter](http://alexgorbatchev.com/SyntaxHighlighter/).
+
+## License
+Exemplar is distributed under the MIT license as tweetware. If you like it, please tweet about it.
+SyntaxHighlighter is distributed under the MIT license as donationware. If you like it, please [donate](http://alexgorbatchev.com/SyntaxHighlighter/donate.html).
+
+## Contributors
+Exemplar was written by Jason Stehle.
---
+++ dist/exemplar.js
@@ -0,0 +1,61 @@
+//     Exemplar.js 0.1.0
+//     (c) 2013 Jason Stehle, http://www.codistic.com/projects/exemplar/
+//     Exemplar.js may be freely distributed under the MIT license.
+
+(function (global) {
+	var successes = [], failures = [];
+	
+	function testExemplar ($elm, result, index) {
+		var $expectedElm = $elm.find('script[type=exemplar-expected-output]');
+		
+		if ($expectedElm.length) {
+			var expected = $expectedElm.html().trim();
+			var properties = { index: index, expected: expected, result: result };
+			
+			if (result === expected) {
+				successes.push(properties);
+			} else {
+				failures.push(properties);
+				$elm.addClass('test-failure');
+			}
+		}
+	}
+	
+	function displayTestResults () {
+		var successCount = successes.length, failureCount = failures.length;
+		if (failureCount) {
+			var message = failureCount + '/' + (successCount + failureCount) + ' failed.';
+			$('.exemplar-test-results').text(message).show();
+		}
+	}
+
+	var exemplar = {
+		init: function () {
+			
+			$('pre[data-exemplar-pull]').each(function (idx, elm) {
+				var $elm = $(elm);
+				var selector = $elm.data('exemplar-pull');
+				$elm.text($(selector).html());
+			});
+			
+			$('div[data-exemplar-type]').each(function (idx, elm) {
+				var $elm = $(elm);
+				var exemplarType = $elm.data('exemplar-type');
+				var src = $elm.find('script[type=exemplar]').html().replace(/\s+$/g, ''); //strip trailing line breaks
+				var func = new Function(src);
+				var result = (exemplarType === 'js') ? JSON.stringify(func()) : func();
+		
+				$elm.find('pre[data-exemplar-target]').html(src);
+				$elm.find('*[data-exemplar-output]').html(result);
+				testExemplar($elm, result);
+			});
+			
+			SyntaxHighlighter.defaults.toolbar = false;
+			SyntaxHighlighter.all();
+		
+			displayTestResults();
+		}
+	};
+	
+	global.exemplar = exemplar;
+})(this);
